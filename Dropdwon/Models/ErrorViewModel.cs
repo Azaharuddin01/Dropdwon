11dbfce7381872ccaa79c29719a3ea8c5f5d56e7
user <user@DESKTOP-3G4HHUT>
2022-03-06 03:43:27 -0800

    Add project files.

---
+++ Dropdwon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dropdwon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
